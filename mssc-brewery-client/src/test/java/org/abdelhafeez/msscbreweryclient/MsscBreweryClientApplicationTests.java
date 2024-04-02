1519515a055ab892a26b87b690e1bd2e7ef79e49
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-02 03:24:02 +0200

    init project

---
+++ mssc-brewery-client/src/test/java/org/abdelhafeez/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abdelhafeez.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
